1ab4233626a25e789151772fe09ecc8193f4148e
Vlad1sl4v <vlad0109031@gmail.com>
2023-06-23 12:18:44 +0300

    edited task

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 const array = [1, 2, 3, 4, 5, 6, 7];
 
 function removeElement(array, item) {
-    array.splice(array.indexOf(item), 1);
+    array.splice(array.indexOf(item), 1); //Це якщо треба видалити конкретний елемент, наприклад елемент п'ять.
+    // array.splice(item - 1, 1); // Це якщо нам потрібно видалити, наприклад, п'ятий елемент за списком. 
 }
 
 removeElement(array, 5);
